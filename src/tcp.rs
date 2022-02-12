8d51598a47fa9b90c008926e1d0de660a47cc012
tmoka0476 <knowledzr+alt@gmail.com>
2022-02-12 14:14:32 +0900

    Add listen handler

--- src/tcp.rs
+++ src/tcp.rs
@@ -159,6 +159,8 @@ impl TCP {
             }
             let sock_id = socket.get_sock_id();
             if let Err(error) = match socket.status {
+                TcpStatus::Listen => self.listen_handler(table, sock_id, &packet, remote_addr),
+                TcpStatus::SynRcvd => self.synrcvd_handler(table, sock_id, &packet),
                 TcpStatus::SynSent => self.synsent_handler(socket, &packet),
                 _ => {
                     dbg!("not implemented state");
@@ -195,6 +197,49 @@ impl TCP {
             .connected_connection_queue
             .pop_front()
             .context("no connected socket")?)
+    }
+
+    /// LISTEN状態のソケットに到着したパケットの処理
+    fn listen_handler(
+        &self,
+        mut table: RwLockWriteGuard<HashMap<SockID, Socket>>,
+        listening_socket_id: SockID,
+        packet: &TCPPacket,
+        remote_addr: Ipv4Addr,
+    ) -> Result<()> {
+        dbg!("listen handler");
+        if packet.get_flag() & tcpflags::ACK > 0 {
+            // 本来ならRSTをsendする
+            return Ok(());
+        }
+        let listening_socket = table.get_mut(&listening_socket_id).unwrap();
+        if packet.get_flag() & tcpflags::SYN > 0 {
+            // passive openの処理
+            // 後に接続済みソケットとなるソケットを新たに生成する
+            let mut connection_socket = Socket::new(
+                listening_socket.local_addr,
+                remote_addr,
+                listening_socket.local_port,
+                packet.get_src(),
+                TcpStatus::SynRcvd,
+            )?;
+            connection_socket.recv_param.next = packet.get_seq() + 1;
+            connection_socket.recv_param.initial_seq = packet.get_seq();
+            connection_socket.send_param.initial_seq = rand::thread_rng().gen_range(1..1 << 31);
+            connection_socket.send_param.window = packet.get_window_size();
+            connection_socket.send_tcp_packet(
+                connection_socket.send_param.initial_seq,
+                connection_socket.recv_param.next,
+                tcpflags::SYN | tcpflags::ACK,
+                &[],
+            )?;
+            connection_socket.send_param.next = connection_socket.send_param.initial_seq + 1;
+            connection_socket.send_param.unacked_seq = connection_socket.send_param.initial_seq;
+            connection_socket.listening_socket = Some(listening_socket.get_sock_id());
+            dbg!("status: listen -> ", &connection_socket.status);
+            table.insert(connection_socket.get_sock_id(), connection_socket);
+        }
+        Ok(())
     }
 
     pub fn new() -> Arc<Self> {
